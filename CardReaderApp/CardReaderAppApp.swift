d59d1e7c75c943d07234448283a4d8a32baeba89
Katios <kate.chonkina@gmail.com>
2021-12-15 09:37:18 +0600

    Initial Commit

---
+++ CardReaderApp/CardReaderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardReaderAppApp.swift
+//  CardReaderApp
+//
+//  Created by Екатерина Чернова on 15.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct CardReaderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
